db6bcd5753117e4e74d09d517e2b33815b5bf3b5
7d7y6jky72-cyber <7d7y6jky72@privaterelay.appleid.com>
2026-08-26 16:30:06 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,10 @@
-const CACHE="40k-board-v05";const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match("./index.html")))));
+const CACHE = "40k-board-v070";
+const ASSETS = ["./", "./index.html", "./README.md", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener("fetch", event => {
+ if (event.request.method !== "GET") return;
+ event.respondWith(fetch(event.request).then(response => {
+  const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response;
+ }).catch(() => caches.match(event.request).then(response => response || caches.match("./index.html"))));
+});
